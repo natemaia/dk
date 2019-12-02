ac400d233e3d4ccd3a4c1bd42e0cb1f95eb4e0e1
natemaia <natemaia10@gmail.com>
2019-12-01 20:26:24 -0800

    Minor tweaks

--- yaswm.c
+++ yaswm.c
@@ -23,22 +23,17 @@
 #include <X11/keysym.h>
 #include <X11/cursorfont.h>
 
-#define BORDER   2
+#define BORDER        1
 #define FOCUSCOL      0x999999
 #define UNFOCUSCOL    0x000000
-#define EVENT(e)      (e & ~0x80)
-#define KEY(k)        (k & ~(numlockmask|XCB_MOD_MASK_LOCK))
-#define LEN(x)        (sizeof(x) / sizeof(x[0]))
 #define MODKEY        XCB_MOD_MASK_1
+#define LEN(x)        (sizeof(x) / sizeof(x[0]))
+#define KEY(k)        (k & ~(numlockmask|XCB_MOD_MASK_LOCK))
 #define BUTTONMASK    XCB_EVENT_MASK_BUTTON_PRESS|XCB_EVENT_MASK_BUTTON_RELEASE
 #define MOTIONMASK    XCB_EVENT_MASK_BUTTON_RELEASE|XCB_EVENT_MASK_BUTTON_MOTION|XCB_EVENT_MASK_POINTER_MOTION_HINT
 
-/* commands */
-const char *termcmd[]  = { "st", NULL };
-const char *dmenucmd[] = { "dmenu_run", NULL };
-
-typedef struct Key Key;
 typedef union Arg Arg;
+typedef struct Key Key;
 
 union Arg {
 	int i;
@@ -57,6 +52,8 @@ struct Key {
 enum { INACTIVE, ACTIVE };
 enum { CurNormal, CurMove, CurResize, CurLast };
 
+unsigned int numlockmask = 0;
+
 static xcb_connection_t  *conn;
 static xcb_screen_t      *scr;
 static xcb_window_t      focuswin;
@@ -65,44 +62,24 @@ static xcb_key_symbols_t *syms = NULL;
 static xcb_cursor_t      cursor[CurLast];
 static xcb_generic_error_t *error = NULL;
 
-unsigned int numlockmask = 0;
-
-static int deploy(void);
+static void deploy(void);
 static void cleanup(void);
 static void spawn(const Arg *arg);
 static void focus(xcb_window_t, int);
 static void subscribe(xcb_window_t);
 static void quit(const Arg *arg);
 
-/* static const handler_func_t handler_funs[] = { */
-/* 	{ XCB_BUTTON_PRESS, buttonpress }, */
-/* 	{ XCB_CLIENT_MESSAGE, clientmessage }, */
-/* 	{ XCB_CONFIGURE_REQUEST, configurerequest }, */
-/* 	{ XCB_CONFIGURE_NOTIFY, configurenotify }, */
-/* 	{ XCB_DESTROY_NOTIFY, destroynotify }, */
-/* 	{ XCB_ENTER_NOTIFY, enternotify }, */
-/* 	{ XCB_EXPOSE, expose }, */
-/* 	{ XCB_FOCUS_IN, focusin }, */
-/* 	{ XCB_KEY_PRESS, keypress }, */
-/* 	{ XCB_MAPPING_NOTIFY, mappingnotify }, */
-/* 	{ XCB_MAP_REQUEST, maprequest }, */
-/* 	{ XCB_PROPERTY_NOTIFY, propertynotify }, */
-/* 	{ XCB_UNMAP_NOTIFY, unmapnotify }, */
-/* 	{ XCB_NONE, NULL } */
-/* }; */
-
 const Key keys[] = {
 	/* modifier                  key        function   arg */
-	{ MODKEY,                    XK_p,      spawn,    {.v = dmenucmd } },
-	{ MODKEY|XCB_MOD_MASK_SHIFT, XK_Return, spawn,    {.v = termcmd } },
 	{ MODKEY|XCB_MOD_MASK_SHIFT, XK_q,      quit,     {0} },
+	{ MODKEY|XCB_MOD_MASK_SHIFT, XK_Return, spawn,    {.v = (char *[]){"st", NULL}} },
+	{ MODKEY,                    XK_p,      spawn,    {.v = (char *[]){"dmenu_run", NULL}} },
 };
 
 static void testerr(const char* file, const int line)
 {
 	if (error) {
-		fprintf(stderr, "%s:%d - request returned error %i, \"%s\"\n", file, line,
-			(int)error->error_code, xcb_event_get_error_label(error->error_code));
+		fprintf(stderr, "%s:%d - request returned error %i, \"%s\"\n", file, line, (int)error->error_code, xcb_event_get_error_label(error->error_code));
 		free(error);
 		error = NULL;
 		assert(0);
@@ -112,7 +89,7 @@ static void testerr(const char* file, const int line)
 static void sigchld(int unused)
 {
 	if (signal(SIGCHLD, sigchld) == SIG_ERR)
-		errx(1, "Can't install SIGCHLD handler");
+		errx(1, "can't install SIGCHLD handler");
 	while(0 < waitpid(-1, NULL, WNOHANG));
 }
 
@@ -120,12 +97,8 @@ static void checkotherwm(void)
 { /* this should cause an error if some other window manager is running */
 	uint32_t values[] = { XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT };
 	xcb_void_cookie_t wm_cookie = xcb_change_window_attributes_checked(conn, root, XCB_CW_EVENT_MASK, values);
-	if ((error = xcb_request_check(conn, wm_cookie))) {
-		fprintf(stderr, "error code: %i\n", error->error_code);
-		free(error);
-		error = NULL;
-		errx(1, "wm: another window manager is already running\n");
-	}
+	if (xcb_request_check(conn, wm_cookie))
+		errx(1, "another window manager is already running");
 }
 
 static void quit(const Arg *arg)
@@ -225,19 +198,13 @@ static void cleanup(void)
 		xcb_disconnect(conn);
 }
 
-static int deploy(void)
+static void deploy(void)
 {
 	sigchld(0);
 	initcursor();
 	focuswin = scr->root;
 	syms = xcb_key_symbols_alloc(conn);
-	uint32_t cw_values[] = { /* select for events */
-		XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY,
-			/* |XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT|XCB_EVENT_MASK_BUTTON_PRESS| */
-			/* XCB_EVENT_MASK_ENTER_WINDOW|XCB_EVENT_MASK_LEAVE_WINDOW|XCB_EVENT_MASK_STRUCTURE_NOTIFY| */
-			/* XCB_EVENT_MASK_PROPERTY_CHANGE, */
-		cursor[CurNormal]
-	};
+	uint32_t cw_values[] = { XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY, cursor[CurNormal] };
 	xcb_void_cookie_t cookie = xcb_change_window_attributes_checked(conn, root, XCB_CW_EVENT_MASK|XCB_CW_CURSOR, cw_values);
 	if ((error = xcb_request_check(conn, cookie)))
 		testerr(__FILE__, __LINE__);
@@ -245,8 +212,6 @@ static int deploy(void)
 	grabkeys();
 	grabbuttons();
 	xcb_flush(conn);
-
-	return 0;
 }
 
 static void focus(xcb_window_t win, int mode)
@@ -254,7 +219,6 @@ static void focus(xcb_window_t win, int mode)
 	uint32_t values[1];
 	values[0] = mode ? FOCUSCOL : UNFOCUSCOL;
 	xcb_change_window_attributes(conn, win, XCB_CW_BORDER_PIXEL, values);
-
 	if (mode == ACTIVE) {
 		xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT, win, XCB_CURRENT_TIME);
 		if (win != focuswin) {
@@ -275,122 +239,118 @@ static void subscribe(xcb_window_t win)
 	xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_BORDER_WIDTH, values);
 }
 
-static void events_loop(void)
+static void event_loop(void)
 {
 	uint32_t values[3];
 	xcb_window_t win = 0;
 	xcb_generic_event_t *ev;
 	xcb_get_geometry_reply_t *geom;
 
-	for (;;) {
-		if (!(ev = xcb_wait_for_event(conn)))
-			errx(1, "xcb connection broken");
-		switch (EVENT(ev->response_type)) {
-			case XCB_CREATE_NOTIFY:
-				{
-					xcb_create_notify_event_t *e = (xcb_create_notify_event_t *)ev;
-					if (e && !e->override_redirect) {
-						subscribe(e->window);
-						focus(e->window, ACTIVE);
-					}
+	while ((ev = xcb_wait_for_event(conn))) {
+		switch (ev->response_type & ~0x80) {
+			case XCB_CREATE_NOTIFY: {
+				xcb_create_notify_event_t *e = (xcb_create_notify_event_t *)ev;
+				if (e && !e->override_redirect) {
+					subscribe(e->window);
+					focus(e->window, ACTIVE);
 				}
 				break;
-			case XCB_DESTROY_NOTIFY:
-				{
-					xcb_destroy_notify_event_t *e = (xcb_destroy_notify_event_t *)ev;
-					xcb_kill_client(conn, e->window);
+			}
+			case XCB_DESTROY_NOTIFY: {
+				xcb_destroy_notify_event_t *e = (xcb_destroy_notify_event_t *)ev;
+				xcb_kill_client(conn, e->window);
+				break;
+			}
+			case XCB_ENTER_NOTIFY: {
+				xcb_enter_notify_event_t *e = (xcb_enter_notify_event_t *)ev;
+				focus(e->event, ACTIVE);
+				break;
+			}
+			case XCB_MAP_NOTIFY: {
+				xcb_map_notify_event_t *e = (xcb_map_notify_event_t *)ev;
+				if (!e->override_redirect) {
+					xcb_map_window(conn, e->window);
+					focus(e->window, ACTIVE);
 				}
 				break;
-			case XCB_ENTER_NOTIFY:
-				{
-					xcb_enter_notify_event_t *e = (xcb_enter_notify_event_t *)ev;
-					focus(e->event, ACTIVE);
-				}
-				break;
-			case XCB_MAP_NOTIFY:
-				{
-					xcb_map_notify_event_t *e = (xcb_map_notify_event_t *)ev;
-					if (!e->override_redirect) {
-						xcb_map_window(conn, e->window);
-						focus(e->window, ACTIVE);
-					}
-				}
-				break;
+			}
 			case XCB_KEY_PRESS:
 				keypress(ev);
 				break;
-			case XCB_BUTTON_PRESS:
-				{
-					xcb_button_press_event_t *b = (xcb_button_press_event_t *)ev;
-					win = b->child;
-					if (!win || win == scr->root)
-						break;
-					values[0] = XCB_STACK_MODE_ABOVE;
-					xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_STACK_MODE, values);
+			case XCB_BUTTON_PRESS: {
+				xcb_button_press_event_t *b = (xcb_button_press_event_t *)ev;
+				win = b->child;
+				if (!win || win == scr->root)
+					break;
+				values[0] = XCB_STACK_MODE_ABOVE;
+				xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_STACK_MODE, values);
+				geom = xcb_get_geometry_reply(conn, xcb_get_geometry(conn, win), NULL);
+				if (b->detail == 1) {
+					values[2] = 1;
+					xcb_warp_pointer(conn, XCB_NONE, win, 0, 0, 0, 0, geom->width/2, geom->height/2);
+				} else {
+					values[2] = 3;
+					xcb_warp_pointer(conn, XCB_NONE, win, 0, 0, 0, 0, geom->width, geom->height);
+				}
+				xcb_grab_pointer(conn, 0, scr->root, MOTIONMASK, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC,
+						scr->root, cursor[b->detail == 1 ? CurMove : CurResize], XCB_CURRENT_TIME);
+				xcb_flush(conn);
+				break;
+			}
+			case XCB_MOTION_NOTIFY: {
+				xcb_query_pointer_reply_t *p = xcb_query_pointer_reply(conn, xcb_query_pointer(conn, scr->root), 0);
+				if (values[2] == 1) {
 					geom = xcb_get_geometry_reply(conn, xcb_get_geometry(conn, win), NULL);
-					if (b->detail == 1) {
-						values[2] = 1;
-						xcb_warp_pointer(conn, XCB_NONE, win, 0, 0, 0, 0, geom->width/2, geom->height/2);
-					} else {
-						values[2] = 3;
-						xcb_warp_pointer(conn, XCB_NONE, win, 0, 0, 0, 0, geom->width, geom->height);
-					}
-					xcb_grab_pointer(conn, 0, scr->root, MOTIONMASK, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, scr->root, cursor[b->detail == 1 ? CurMove : CurResize], XCB_CURRENT_TIME);
+					if (!geom)
+						break;
+					values[0] = (p->root_x + geom->width / 2 > scr->width_in_pixels - (BORDER*2))
+						? scr->width_in_pixels - geom->width - (BORDER*2)
+						: p->root_x - geom->width / 2;
+					values[1] = (p->root_y + geom->height / 2 > scr->height_in_pixels - (BORDER*2))
+						? (scr->height_in_pixels - geom->height - (BORDER*2))
+						: p->root_y - geom->height / 2;
+					if (p->root_x < geom->width/2)
+						values[0] = 0;
+					if (p->root_y < geom->height/2)
+						values[1] = 0;
+					xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_X|XCB_CONFIG_WINDOW_Y, values);
+					xcb_flush(conn);
+				} else if (values[2] == 3) {
+					geom = xcb_get_geometry_reply(conn, xcb_get_geometry(conn, win), NULL);
+					values[0] = p->root_x - geom->x;
+					values[1] = p->root_y - geom->y;
+					xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_WIDTH|XCB_CONFIG_WINDOW_HEIGHT, values);
 					xcb_flush(conn);
 				}
 				break;
-			case XCB_MOTION_NOTIFY:
-				{
-					xcb_query_pointer_reply_t *p = xcb_query_pointer_reply(conn, xcb_query_pointer(conn, scr->root), 0);
-					if (values[2] == 1) {
-						geom = xcb_get_geometry_reply(conn, xcb_get_geometry(conn, win), NULL);
-						if (!geom)
-							break;
-						values[0] = (p->root_x + geom->width / 2 > scr->width_in_pixels - (BORDER*2)) ? scr->width_in_pixels - geom->width - (BORDER*2) : p->root_x - geom->width / 2;
-						values[1] = (p->root_y + geom->height / 2 > scr->height_in_pixels - (BORDER*2)) ? (scr->height_in_pixels - geom->height - (BORDER*2)) : p->root_y - geom->height / 2;
-						if (p->root_x < geom->width/2)
-							values[0] = 0;
-						if (p->root_y < geom->height/2)
-							values[1] = 0;
-						xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_X|XCB_CONFIG_WINDOW_Y, values);
-						xcb_flush(conn);
-					} else if (values[2] == 3) {
-						geom = xcb_get_geometry_reply(conn, xcb_get_geometry(conn, win), NULL);
-						values[0] = p->root_x - geom->x;
-						values[1] = p->root_y - geom->y;
-						xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_WIDTH|XCB_CONFIG_WINDOW_HEIGHT, values);
-						xcb_flush(conn);
-					}
-				}
-				break;
+			}
 			case XCB_BUTTON_RELEASE:
 				focus(win, ACTIVE);
 				xcb_ungrab_pointer(conn, XCB_CURRENT_TIME);
 				break;
-			case XCB_CONFIGURE_NOTIFY:
-				{
-					xcb_configure_notify_event_t *e = (xcb_configure_notify_event_t *)ev;
-					if (e->window != focuswin)
-						focus(e->window, INACTIVE);
-					focus(focuswin, ACTIVE);
-				}
+			case XCB_CONFIGURE_NOTIFY: {
+				xcb_configure_notify_event_t *e = (xcb_configure_notify_event_t *)ev;
+				if (e->window != focuswin)
+					focus(e->window, INACTIVE);
+				focus(focuswin, ACTIVE);
 				break;
+			}
 		}
 		xcb_flush(conn);
 		free(ev);
 	}
+	errx(1, "xcb connection broken");
 }
 
 int main(void)
 {
 	atexit(cleanup);
 	if (xcb_connection_has_error((conn = xcb_connect(NULL, NULL))))
-		errx(EXIT_FAILURE, "error connecting to X");
+		errx(1, "error connecting to X");
 	scr = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
 	root = scr->root;
 	checkotherwm();
 	deploy();
-	for (;;)
-		events_loop();
-	return EXIT_FAILURE;
+	event_loop();
+	return 1;
 }

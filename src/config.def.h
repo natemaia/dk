b7f81297bcd950d26046c2adc98747aa814f0cc1
natemaia <natemaia10@gmail.com>
2020-04-05 13:45:29 -0700

    Update config.def.h

--- src/config.def.h
+++ src/config.def.h
@@ -1,21 +1,26 @@
-/* In order to configure yaxwm, copy this file to config.h and edit it
- * see license file for copyright and license details
- * vim:ft=c:fdm=syntax:ts=4:sts=4:sw=4
- */
+/* In order to add callbacks or layouts to yaxwm,
+ * copy this file to config.h and edit it
+ *
+ * see license file for copyright and license details */
 
 
-/* simple example of a callback function for album art windows */
+/* example of a simple callback for album art windows */
 static void albumart(Client *c)
-{ /* add padding for the window width + gaps,
-	 move window to the left center of the screen,
-	 focus the last focused window */
+{
+	/* add padding to fit the window on the right */
 	c->ws->padr = c->w + (2 * c->ws->gappx);
-	gravitate(c, Center, Right, 1);
+
+	/* move window to the right center of the screen, */
+	gravitate(c, Right, Center, 1);
+
+	/* focus the previously focused window (stack next) */
 	focus(c->snext);
+
+	/* apply changes and layout the workspace */
 	layoutws(c->ws);
 }
 
-/* callbacks recognized for use with window rules.
+/* "callback" names recognized for use with rules.
  * Callback functions have the following prototype: void function(Client *); */
 static Callback callbacks[] = {
 	{ "albumart", albumart },
@@ -29,22 +34,3 @@ static Layout layouts[] = {
 	{ "mono", mono },
 	{ "none", NULL },
 };
-
-
-/* Basic configuration still not handled by commands */
-static WsRule wsrules[] = {
-	/* workspace default settings and how many to allocate if more are
-	 * allocated later the values from the first rule will be use
-	 * name, master, stack, gap, split, ssplit, padr, padl, padt, padb,  layout,     next */
-	{ "1",     1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-	{ "2",     1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-	{ "3",     1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-	{ "4",     1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-	{ "5",     1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-	{ "6",     1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-	{ "7",     1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-	{ "8",     1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-	{ "9",     1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-	{ "10",    1,      3,    0,  0.55,   0.55,   0,    0,    0,    0,   &layouts[0], NULL },
-};
-

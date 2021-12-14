9abd0674feac5cc0c386a8656fc989b1bf15b757
natemaia <natemaia10@gmail.com>
2021-12-13 19:54:59 -0800

    Force update of win, layout, and ws status when initiated.

--- src/cmd.c
+++ src/cmd.c
@@ -899,11 +899,11 @@ int cmdstatus(char **argv)
 	while (*argv) {
 		if (!strcmp("type", *argv)) {
 			argv++, nparsed++;
-			if (!strcmp("ws", *argv))          s.type = STAT_WS;
+			if (!strcmp("ws", *argv))          s.type = STAT_WS, wschange = 1;
 			else if (!strcmp("bar", *argv))    s.type = STAT_BAR;
-			else if (!strcmp("win", *argv))    s.type = STAT_WIN;
+			else if (!strcmp("win", *argv))    s.type = STAT_WIN, winchange = 1;
 			else if (!strcmp("full", *argv))   s.type = STAT_FULL;
-			else if (!strcmp("layout", *argv)) s.type = STAT_LYT;
+			else if (!strcmp("layout", *argv)) s.type = STAT_LYT, lytchange = 1;
 			else                               goto badvalue;
 		} else if (!strcmp("num", *argv)) {
 			argv++, nparsed++;

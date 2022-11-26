d3ec17ecc086c352c70e8df1fd4806971f6a5279
natemaia <natemaia10@gmail.com>
2022-11-25 23:11:27 -0800

    Fix/Revert: mono layout not resizing all clients

--- src/layout.c
+++ src/layout.c
@@ -240,12 +240,9 @@ int mono(Workspace *ws)
 		int b = globalcfg[GLB_SMART_BORDER].val ? 0 : ws->sel->bw;
 
 		for (c = nexttiled(ws->clients); c; c = nexttiled(c->next)) {
-			if (c == selws->sel)
-				resizehint(c, ws->mon->wx + ws->padl + g, ws->mon->wy + ws->padt + g,
-						ws->mon->ww - ws->padl - ws->padr - (2 * g) - (2 * b),
-						ws->mon->wh - ws->padt - ws->padb - (2 * g) - (2 * b), b, 0, 0);
-			else
-				MOVE(c->win, W(c) * -2, c->y);
+			resizehint(c, ws->mon->wx + ws->padl + g, ws->mon->wy + ws->padt + g,
+					ws->mon->ww - ws->padl - ws->padr - (2 * g) - (2 * b),
+					ws->mon->wh - ws->padt - ws->padb - (2 * g) - (2 * b), b, 0, 0);
 		}
 		xcb_aux_sync(con);
 	}
